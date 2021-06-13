f28d7107d3bed60cef405ec1d54e3f7f7ceba350
Nabilah Daud <48626338+naveylah@users.noreply.github.com>
2021-06-14 00:20:06 +0800

    Add files via upload

--- lib/gridone.dart
+++ lib/gridone.dart
@@ -5,27 +5,102 @@ class gridOne extends StatefulWidget {
   _gridOneState createState() => _gridOneState();
 }
 
+final formKey = GlobalKey<FormState>();
+bool _status = false;
+bool _isSwitched = false;
+
 // ignore: camel_case_types
 class _gridOneState extends State<gridOne> {
   @override
   Widget build(BuildContext context) {
+    var car_rental_outlined;
     return Scaffold(
-        body: GridView.count(
-            crossAxisCount: 3,
-            children: List.generate(5, (index) {
-              return new Card(
-                  elevation: 10.0,
-                  margin: EdgeInsets.all(7.0),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20.0)),
-                  child: Container(
-                    child: Align(
-                        alignment: Alignment.center,
-                        child: Text(
-                          "$index",
-                          style: TextStyle(fontSize: 24.0),
-                        )),
-                  ));
-            })));
+        appBar: AppBar(
+          title: Text("MAD Assignment"),
+          backgroundColor: Colors.lime[800],
+        ),
+        body: Center(
+            child: GridView.count(crossAxisCount: 3, children: <Widget>[
+          Container(
+            margin: EdgeInsets.all(10.0),
+            child: Card(
+              elevation: 10.0,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0)),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(height: 5.0),
+                    IconButton(
+                        icon: Icon(Icons.analytics_outlined),
+                        iconSize: 70.0,
+                        onPressed: () {
+                          setState(() {
+                            _status = true;
+                          });
+                        }),
+                    Text(
+                      "Fan",
+                      style: TextStyle(fontSize: 15),
+                    ),
+                    Text("$_status")
+                  ]),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(10.0),
+            child: Card(
+              elevation: 10.0,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0)),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(height: 5.0),
+                    IconButton(
+                        icon: Icon(Icons.bathtub_outlined),
+                        iconSize: 70.0,
+                        onPressed: () {
+                          setState(() {
+                            _status = true;
+                          });
+                        }),
+                    Text(
+                      "Toilet Light",
+                      style: TextStyle(fontSize: 15),
+                    ),
+                    Text("$_status")
+                  ]),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(10.0),
+            child: Card(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0)),
+              elevation: 10.0,
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(height: 5.0),
+                    IconButton(
+                        icon: Icon(Icons.car_rental),
+                        iconSize: 70.0,
+                        onPressed: () {
+                          setState(() {
+                            _status = true;
+                          });
+
+                          formKey.currentState.reset();
+                        }),
+                    Text(
+                      "Garage",
+                      style: TextStyle(fontSize: 15),
+                    ),
+                    Text("$_status")
+                  ]),
+            ),
+          ),
+        ])));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,27 @@
 import 'package:flutter/material.dart';
-import './gridLayout.dart';
+import 'package:test_one/gridone.dart';
+import 'package:test_one/testone.dart';
+import 'gridone.dart' as gridone;
 
 void main() => runApp(
-      MaterialApp(home: gridLayout()),
+      MaterialApp(home: gridOne()),
     );
+
+//output an AppBar and some text below it (i.e. in the body of the page)
+//Add a button (e.g. RaisedButton) which changes the text (to any other text of your choice)
+//Split the app into three custom widgets: App, Button & Text (three different files.
+
+  // class myButton extends StatelessWidget {
+  // final Function handler;
+  // final String mytext;
+
+  // myButton(this.mytext, this.handler);
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Container(
+  //     child: RaisedButton(
+  //       child: Text(mytext),
+  //     ),
+  //   );
+  // }
